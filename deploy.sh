229f738133f6a0700b138cb4321138fbc4ddd932
unknown <jamiearmoordon@yahoo.com>
2026-04-10 12:50:22 +0100

    fix: add /webhooks/ proxy to Nginx config for Stripe webhook delivery

--- deploy.sh
+++ deploy.sh
@@ -94,6 +94,15 @@ server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
+
+    # Stripe Webhooks Reverse Proxy
+    location /webhooks/ {
+        proxy_pass http://127.0.0.1:8000/webhooks/;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
 }
 EOL
 
